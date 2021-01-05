863ef1818373bae49c33953cbea9aa94363b5ca9
Matthew Ricklefs <72839408+matthewricklefs@users.noreply.github.com>
2021-01-05 16:29:03 -0500

    introducing setInterval and the Date() to create a steady ticking clock

--- src/App.js
+++ src/App.js
@@ -4,14 +4,24 @@ import "./styles.css";
 function Counter() {
   const [count, setCount] = useState(0);
   const [color, setColor] = useState("salmon");
+  const [time, setTime] = useState(Date.now());
 
   useEffect(() => {
     console.log(`I'm in  a useEffect hook, count is ${count}`);
+
+    return () => console.log("I'm cleaning up the count effect");
   }, [count]);
 
   useEffect(() => {
     console.log(`I'm in another useEffect hook, count is ${color}`);
   }, [color]);
+
+  useEffect(() => {
+    const myInterval = setInterval(() => setTime(Date.now()), 1000);
+    setTime(Date.now());
+
+    return clearInterval(myInterval);
+  }, [time]);
 
   const handleColorChange = () => {
     const nextColor = color === "salmon" ? "gold" : "salmon";
@@ -25,6 +35,7 @@ function Counter() {
       <button onClick={handleColorChange}>Change Color</button>
       <button onClick={() => setCount(count - 1)}>Decrease</button>
       <h1 style={{ color }}> {count} </h1>
+      <h1>{time}</h1>
     </div>
   );
 }
